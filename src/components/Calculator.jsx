1f36c3de7cc02c2dce0aa4bdf1d253064b1b3c9e
MacBookPro <matthewpalmer9@outlook.com>
2021-03-02 15:48:43 -0600

    Added an operator handler that will not allow back-to-back operators. This is to avoid operational errors.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -10,6 +10,19 @@ export default function Calculator() {
         setResult(result + e.target.value);
     };
 
+    const handleOperator = e => {
+        let newResult = result.split("");
+        let checker = newResult[newResult.length - 1]
+
+        // Checks if an operator already exists at the end of the operation
+        // If it does, replace it with the one that was clicked
+        if(checker === "+" || checker === "-" || checker === "*" || checker === "/") {
+            newResult.pop()
+        }
+        const joiner = newResult.join("")
+        setResult(joiner + e.target.value)
+    }
+
     const addParenthesis = () => {
         if(!result.includes("(")) {
             setResult(result + "*(")
@@ -37,25 +50,25 @@ export default function Calculator() {
                 <button onClick={clearSession} className="grey">C</button>
                 <button onClick={addParenthesis} className="grey">( )</button>
                 <button className="grey">%</button>
-                <button className="orange">÷</button>
+                <button onClick={handleOperator} className="orange" value="/">÷</button>
 
                 {/* Row 2 */}
                 <button onClick={addToTyped} className="darkgrey" value="7">7</button>
                 <button onClick={addToTyped} className="darkgrey" value="8">8</button>
                 <button onClick={addToTyped} className="darkgrey" value="9">9</button>
-                <button onClick={addToTyped} className="orange" value="*">✕</button>
+                <button onClick={handleOperator} className="orange" value="*">✕</button>
 
                 {/* Row 3 */}
                 <button onClick={addToTyped} className="darkgrey" value="4">4</button>
                 <button onClick={addToTyped} className="darkgrey" value="5">5</button>
                 <button onClick={addToTyped} className="darkgrey" value="6">6</button>
-                <button onClick={addToTyped} className="orange" value="-">-</button>
+                <button onClick={handleOperator} className="orange" value="-">-</button>
 
                 {/* Row 4 */}
                 <button onClick={addToTyped} className="darkgrey" value="1">1</button>
                 <button onClick={addToTyped} className="darkgrey" value="2">2</button>
                 <button onClick={addToTyped} className="darkgrey" value="3">3</button>
-                <button onClick={addToTyped} className="orange" value="+">+</button>
+                <button onClick={handleOperator} className="orange" value="+">+</button>
 
                 {/* Row 5 */}
                 <button className="darkgrey">+/-</button>
