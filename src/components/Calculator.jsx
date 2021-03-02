8b6dbf3bb51442e0cff9fe277ce056daadf1f360
MacBookPro <matthewpalmer9@outlook.com>
2021-03-02 12:56:58 -0600

    Added the ability to type in numbers & auto format with a comma every 3 digits.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,40 +4,50 @@ import '../styles/calculator.css';
 export default function Calculator() {
 
     const [result, setResult] = useState(0);
+    const [typed, setTyped] = useState("");
+
+
+    const addToTyped = e => {
+        setTyped(parseInt(typed + e.target.value))
+    }
+
+    const clearSession = () => {
+        setTyped("");
+    };
 
     return (
         <div className="calculator-container">
             <div className="result-box">
-                {result}
+                {typed === "" ? "0" : typed.toLocaleString("en-US", {maximumFractionDigits:2})}
             </div>
             <div className="ios-buttons">
                 {/* Row 1 */}
-                <button className="grey">C</button>
+                <button onClick={clearSession} className="grey">C</button>
                 <button className="grey">( )</button>
                 <button className="grey">%</button>
                 <button className="orange">÷</button>
 
                 {/* Row 2 */}
-                <button className="darkgrey">7</button>
-                <button className="darkgrey">8</button>
-                <button className="darkgrey">9</button>
-                <button className="orange">✕</button>
+                <button onClick={addToTyped} className="darkgrey" value="7">7</button>
+                <button onClick={addToTyped} className="darkgrey" value="8">8</button>
+                <button onClick={addToTyped} className="darkgrey" value="9">9</button>
+                <button onClick={addToTyped} className="orange" value="✕">✕</button>
 
                 {/* Row 3 */}
-                <button className="darkgrey">4</button>
-                <button className="darkgrey">5</button>
-                <button className="darkgrey">6</button>
-                <button className="orange">-</button>
+                <button onClick={addToTyped} className="darkgrey" value="4">4</button>
+                <button onClick={addToTyped} className="darkgrey" value="5">5</button>
+                <button onClick={addToTyped} className="darkgrey" value="6">6</button>
+                <button onClick={addToTyped} className="orange" value="-">-</button>
 
                 {/* Row 4 */}
-                <button className="darkgrey">1</button>
-                <button className="darkgrey">2</button>
-                <button className="darkgrey">3</button>
-                <button className="orange">+</button>
+                <button onClick={addToTyped} className="darkgrey" value="1">1</button>
+                <button onClick={addToTyped} className="darkgrey" value="2">2</button>
+                <button onClick={addToTyped} className="darkgrey" value="3">3</button>
+                <button onClick={addToTyped} className="orange" value="+">+</button>
 
                 {/* Row 5 */}
                 <button className="darkgrey">+/-</button>
-                <button className="darkgrey">0</button>
+                <button onClick={addToTyped} className="darkgrey">0</button>
                 <button className="darkgrey">.</button>
                 <button className="orange">=</button>
             </div>
