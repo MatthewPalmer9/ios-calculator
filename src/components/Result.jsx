3e554b292396c14094647c2f76c01bc4b4df4f82
MacBookPro <matthewpalmer9@outlook.com>
2021-03-11 10:57:41 -0600

    Removed useRef hook testing.

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,13 +1,11 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 export default function Result(props) {
     const { result } = props;
-    const updatedResult = useRef();
-    updatedResult.current = result;
 
     return (
         <div className="result-box">
-            {updatedResult.current === "" ? "0" : updatedResult.current.toLocaleString("en-US", {maximumFractionDigits:2})}
+            {result.current === "" ? "0" : result.current.toLocaleString("en-US", {maximumFractionDigits:2})}
         </div>
     )
 }
